bbd53bb43328b441ce5b7fc5daea6d55f2a80a1f
Aasmund Eldhuset <aasmund.eldhuset@bekk.no>
2012-03-05 21:58:00 +0100

    Reformatting CommandLineExecutor to use tabs.

--- app/controllers/cmd/CommandLineExecutor.java
+++ app/controllers/cmd/CommandLineExecutor.java
@@ -10,88 +10,87 @@ import java.util.concurrent.TimeoutException;
 import models.cmd.CommandLineResult;
 
 public class CommandLineExecutor {
-  public static CommandLineResult execute(final String[] commandLine,
-                                          final boolean captureOutput,
-                                          final boolean captureError,
-                                          final long timeout)
-                                            throws IOException,
-                                            InterruptedException,
-                                            TimeoutException {
-    Runtime runtime = Runtime.getRuntime();
-    Process process = runtime.exec(commandLine);
-    String stdErr = null, stdOut = null;
+	public static CommandLineResult execute(final String[] commandLine,
+											final boolean captureOutput, 
+											final boolean captureError,
+											final long timeout) 
+													throws IOException, InterruptedException, TimeoutException {
+		Runtime runtime = Runtime.getRuntime();
+		Process process = runtime.exec(commandLine);
+		String stdErr = null, stdOut = null;
+		
+		StringBuffer stdOutCapture = new StringBuffer();
+		StringBuffer stdErrCapture = new StringBuffer();
+		if (captureOutput)
+			new InputStreamHandler(stdOutCapture, process.getInputStream());
+		if (captureError)
+			new InputStreamHandler(stdErrCapture, process.getErrorStream());
+		
+		Worker worker = new Worker(process);
+		worker.start();
+		try {
+			worker.join(timeout); // Wait <= this many ms for worker thread to finish
+			if (worker.exit == null) {
+				throw new TimeoutException();
+			}
+		}
+		catch (InterruptedException ex) {
+			worker.interrupt();
+			Thread.currentThread().interrupt();
+			throw ex;
+		}
+		finally {
+			process.destroy(); // Clean up the running command line process
+		}
+		stdOut = stdOutCapture.toString();
+		stdErr = stdErrCapture.toString();
+		return new CommandLineResult(worker.exit, stdOut, stdErr, worker.duration);
+	}
+	
+	private static class Worker extends Thread {
+		private final Process process;
+		private Integer exit;
+		private Long duration;
+		
+		private Worker(Process process) {
+			this.process = process;
+		}
+		
+		public void run() {
+			long startTime = System.nanoTime();
+			try {
+				exit = process.waitFor();
+				duration = (System.nanoTime() - startTime) / 1000000; // Realistically, we don't have nanosecond accuracy.
+			}
+			catch (InterruptedException ignore) {
+				return;
+			}
+		}
+	}
 
-    StringBuffer stdOutCapture = new StringBuffer();
-    StringBuffer stdErrCapture = new StringBuffer();
-    if (captureOutput) {
-      new InputStreamHandler(stdOutCapture, process.getInputStream());
-    }
-
-    if (captureError) {
-      new InputStreamHandler(stdErrCapture, process.getErrorStream());
-    }
-
-    Worker worker = new Worker(process);
-    worker.start();
-    try {
-      worker.join(timeout);  // Wait <= this many ms for worker thread to finish
-      if (worker.exit == null) {
-        throw new TimeoutException();
-      }
-    } catch (InterruptedException ex) {
-      worker.interrupt();
-      Thread.currentThread().interrupt();
-      throw ex;
-    } finally {
-      process.destroy();  // Clean up the running command line process
-    }
-    stdOut = stdOutCapture.toString();
-    stdErr = stdErrCapture.toString();
-    return new CommandLineResult(worker.exit, stdOut, stdErr, worker.duration);
-  }
-
-  private static class Worker extends Thread {
-    private final Process process;
-    private Integer exit;
-    private Long duration;
-
-    private Worker(Process process) {
-      this.process = process;
-    }
-
-    public void run() {
-      long startTime = System.nanoTime();
-      try { 
-        exit = process.waitFor();
-        duration = (System.nanoTime() - startTime) / 1000000;  // Realistically, we don't have nanosecond accuracy.
-      } catch (InterruptedException ignore) {
-        return;
-      }
-    }  
-  }
-
-  private static class InputStreamHandler extends Thread {     
-    private InputStream stream;
-    private StringBuffer capture;
-
-    public InputStreamHandler(StringBuffer capture, InputStream stream) {
-      this.capture = capture;
-      this.stream = stream;
-      start();
-    }
-
-    public void run() {
-      try {
-        BufferedReader input = new BufferedReader(new InputStreamReader(stream));
-        String line = null;
-        while ((line = input.readLine()) != null) {
-          capture.append(line);
-          capture.append("\n");
-        }
-      } catch(IOException e) {
-        e.printStackTrace();
-        return;
-      }
-    }
-  }
+	private static class InputStreamHandler extends Thread {
+		private InputStream stream;
+		private StringBuffer capture;
+		
+		public InputStreamHandler(StringBuffer capture, InputStream stream) {
+			this.capture = capture;
+			this.stream = stream;
+			start();
+		}
+		
+		public void run() {
+			try {
+				BufferedReader input = new BufferedReader(new InputStreamReader(stream));
+				String line = null;
+				while ((line = input.readLine()) != null) {
+					capture.append(line);
+					capture.append("\n");
+				}
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+				return;
+			}
+		}
+	}
 }
