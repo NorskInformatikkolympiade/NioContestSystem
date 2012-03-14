75fbcfa6ad56462a9fe38b06f8ed259b2ce95607
Aasmund Eldhuset <aasmund.eldhuset@bekk.no>
2012-03-15 00:35:42 +0100

    Filtering out the other users' submissions in the task page when the user is not an admin.

--- app/controllers/Tasks.java
+++ app/controllers/Tasks.java
@@ -1,7 +1,9 @@
 package controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import models.Contestant;
 import models.Submission;
 import models.Task;
 import play.mvc.Controller;
@@ -16,6 +18,17 @@ public class Tasks extends Controller {
 	
 	public static void show(Long id) {
 		Task task = Task.findById(id);
-		render(task);
+		Contestant currentContestant = Security.getCurrentContestant();
+		List<Submission> submissions;
+		if (Security.getCurrentContestant().isAdmin)
+			submissions = task.submissions;
+		else {
+			submissions = new ArrayList<Submission>();
+			for (Submission submission : task.submissions) {
+				if (submission.contestant.id == currentContestant.id)
+					submissions.add(submission);
+			}
+		}
+		render(task, submissions);
 	}
 }
